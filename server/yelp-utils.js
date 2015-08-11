1c41f5e12a14f7036603a10cba4a024c3870ae1f
Angela Wang <angeladanwang@gmail.com>
2015-08-11 15:25:44 -0700

    get uber prices and 3 yelp restaurants on getRestaurant post

--- server/yelp-utils.js
+++ server/yelp-utils.js
@@ -1,5 +1,5 @@
 var config = require('./config.js');
-var request = require('request');
+var uber = require('./uber-utils.js');
 
 var yelp = require("yelp").createClient({
   consumer_key: config.YELP_CONSUMER_KEY, 
@@ -11,27 +11,48 @@ var yelp = require("yelp").createClient({
 // See http://www.yelp.com/developers/documentation/v2/search_api
 exports.getRestaurants = function(req, res) {
   //need oauth uber token to get price estimates
-  console.log('getting restaurants, will err from invalid params atm');
+  console.log('getting restaurants data and uber prices with stubbed out params');
   //take params from req
   //feed into yelp search
   params = {
-    term: '',
-    limit: 10,
+    term: 'san francisco food',
+    limit: 3,
     category_filter: '',
     radius_filter: 1000,
-    ll: '',
+    ll: '37.7833, -122.4167',
   };
-
+  
   yelp.search(params, function(error, data) {
 
     if (error) {
       console.log(error);
+      res.send(404);
     } else {
-      console.log(data);
-      var dataCoordinates = [];
-      //filter data down to 3
-      //make request to uber api for cost estimates
-      //res.send(data)
+
+      var start = {latitude: 37.7833, longitude: -122.4167};
+
+      var b = data.businesses;
+      var coord1 = b[0]['location']['coordinate'];
+      var coord2 = b[1]['location']['coordinate'];
+      var coord3 = b[2]['location']['coordinate'];
+
+      //callback hell; to be refactored
+      uber.estimatePrice(start, coord1, function(err, price1) {
+        if (err) {console.log(err)};
+
+        uber.estimatePrice(start, coord2, function(err, price2) {
+          if (err) {console.log(err)}
+
+          uber.estimatePrice(start, coord3, function(err, price3) {
+            if (err) {console.log(err)}
+            res.send({
+              YELP: data,
+              UBER: [price1, price2, price3]
+            })
+          });
+      });
+      });
+      
     }
   });
 
