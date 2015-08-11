b2de127edd26f202c0bcf36d2aacd976f7174f9f
yakomaru <parrish.nate@gmail.com>
2015-08-11 09:19:20 -0700

    removed angular scafolding

--- public/app/src/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('HRRUberProject', [
-  'ngRoute',
-  'mobile-angular-ui',
-  'HRRUberProject.controllers.Main'
-])
-
-.config(function($routeProvider) {
-  $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
-});
