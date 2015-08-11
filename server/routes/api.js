5e098228ba47bbed9eb5ed104ec996cc47fbcd86
Angela Wang <angeladanwang@gmail.com>
2015-08-10 23:31:55 -0700

    set up yelp session, organize routes into server folder, make api file

---
+++ server/routes/api.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var yelp = require('../yelp-utils.js')
+/* GET home page. */
+
+router.get('/getRestaurants', yelp.getRestaurants);
+
+router.post('/confirmRestaurant', function(req, res){
+  //route for confirming choice and dispatching uber
+});
+
+router.post('/cancelRide', function(req, res){
+  //route for cancelling uber request
+});
+
+module.exports = router;
