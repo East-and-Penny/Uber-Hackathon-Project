492c367d8eafbb1050752dbb04016d2a5e8056b5
Eric King <e.king117@gmail.com>
2015-08-14 13:45:56 -0400

    Yelp and Uber requests almost live

--- server/routes/api.js
+++ server/routes/api.js
@@ -13,6 +13,7 @@ router.post('/confirmRestaurant', function(req, res){
   //route for confirming choice and dispatching uber
   console.log(req.body);
   request.post(uber + 'v1/requests', {form: {
+// <<<<<<< HEAD
     product_id: req.body.product_id,
     start_latitude: req.body.start_latitude,
     start_longitude: req.body.start_longitude,
@@ -26,7 +27,23 @@ router.post('/confirmRestaurant', function(req, res){
     } else {
       console.log('request denied');
     }
-  })
+  });
+// =======
+//     product_id: req.body.pid,
+//     start_latitude: req.body.startLat,
+//     start_longitude: req.body.starLong,
+//     end_latitude: req.body.endLat,
+//     end_longitude: req.body.endLong,
+//     surge_confirmation_id: req.body.surgeConfirm
+//   }}, function(err, res, body){
+//     if(err) throw err;
+//     if(res.statusCode === 200){
+//       console.log('request confirmed');
+//     } else {
+//       console.log('request denied');
+//     }
+//   });
+// >>>>>>> Yelp and Uber requests almost live
 });
 
 router.post('/cancelRide', function(req, res){
@@ -38,7 +55,7 @@ router.post('/cancelRide', function(req, res){
     } else {
       console.log('request not cancelled?');
     }
-  })
+  });
 });
 
 module.exports = router;
